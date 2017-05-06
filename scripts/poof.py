5c519e76a17c7456c07301891a0bbf20e106e5d5
puppycodes <rye@jodie.io>
2017-05-05 21:06:48 -0700

    add wooey

--- scripts/poof.py
+++ scripts/poof.py
@@ -7,6 +7,17 @@ import plistlib
 import traceback
 import json
 import time
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Find the sum of all the numbers below a certain number.")
+parser.add_argument('--below', help='The number to find the sum of numbers below.', type=int, default=1000)
+
+def main():
+    args = parser.parse_args()
+    s = sum((i for i in range(args.below)))
+    print("Sum =", s)
+    return 0
 
 def getUDID(dsid, mmeFMFAppToken):
     url = 'https://p04-fmfmobile.icloud.com/fmipservice/friends/%s/1/maxCallback/refreshClient' % dsid
---
+++ src/settings/wooey_settings.py
@@ -0,0 +1,30 @@
+from .django_settings import *
+from wooey.version import DJANGO_VERSION, DJ17
+from django.utils.translation import ugettext_lazy as _
+
+INSTALLED_APPS += (
+    # 'corsheaders',
+    'wooey',
+)
+
+LANGUAGES = [
+  ('de', _('German')),
+  ('en', _('English')),
+  ('fr', _('French')),
+  ('ja', _('Japanese')),
+  ('nl', _('Dutch')),
+  ('zh-hans' if DJANGO_VERSION >= DJ17 else 'zh-cn', _('Simplified Chinese')),
+]
+
+NEW_MIDDLEWARE = []
+for i in MIDDLEWARE_CLASSES:
+    NEW_MIDDLEWARE.append(i)
+    if i == 'django.contrib.sessions.middleware.SessionMiddleware':
+        NEW_MIDDLEWARE.append('django.middleware.locale.LocaleMiddleware')
+
+NEW_MIDDLEWARE.append('lostpup.middleware.ProcessExceptionMiddleware')
+MIDDLEWARE_CLASSES = NEW_MIDDLEWARE
+
+PROJECT_NAME = "lostpup"
+WOOEY_CELERY_APP_NAME = 'wooey.celery'
+WOOEY_CELERY_TASKS = 'wooey.tasks'
